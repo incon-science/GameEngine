f7257ed9a4938e26a1376ce392e7da6dd253d9ee
1ConScience <1conscience@gmail.com>
2025-02-03 00:58:19 +0100

    Refactor display settings to use fullscreen mode and hide cursor

--- head.py
+++ head.py
@@ -14,8 +14,6 @@ joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_coun
 camera = pygame.math.Vector2((0, 0))
 vec = pygame.math.Vector2 #2 for two dimensional
 
-WIDTH = 1280
-HEIGHT = 720
 ACC = 0.5
 FRIC = -0.06#-0.12
 FPS = 60
@@ -24,9 +22,12 @@ deadzone = 0.3#for joystick
  
 FramePerSec = pygame.time.Clock()
  
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-pygame.display.set_caption("GameEngine")
+screen = pygame.display.set_mode((0, 0),pygame.NOFRAME,32)
+pygame.mouse.set_visible(False) # Hide cursor here
+pygame.display.set_caption("nowhere")
+infoObject = pygame.display.Info()
+WIDTH = infoObject.current_w
+HEIGHT = infoObject.current_h
 
 image_droite = pygame.image.load("assets/e.png").convert_alpha()
 image_gauche = pygame.image.load("assets/e_inv.png").convert_alpha()
