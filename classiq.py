940d0e2c242111d466038b634f698f3b0ef3e62e
1ConScience <1conscience@gmail.com>
2025-02-03 20:15:40 +0100

    Change platform surface color from white to dark gray for improved visibility

--- classiq.py
+++ classiq.py
@@ -146,7 +146,7 @@ class Platform(pygame.sprite.Sprite):
     def __init__(self,size,pos):
         super().__init__()
         self.surf = pygame.Surface(size)
-        self.surf.fill((255,255,255))
+        self.surf.fill((20,20,20))
         self.rect = self.surf.get_rect(center = pos)
 
     def move(self):
