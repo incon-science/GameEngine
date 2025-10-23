d4545407f9fd9777e7d9734941e77638f7732e31
JSB27lp <1conscienceofficiel@gmail.com>
2025-10-23 15:52:18 +0200

    ds

--- Camera.py
+++ Camera.py
@@ -1,4 +1,6 @@
 from head import *
+from scipy.interpolate import interp1d
+import math
 
 class Camera():
     def __init__(self):
@@ -10,9 +12,8 @@ class Camera():
         self.updateCameraCenterSmooth()
 
     def updateCameraCenterSmooth(self):
-        self.pos_aim.x = round(self.pos_aim.x)
-        self.pos_aim.y = round(self.pos_aim.y)
 
+        """
         if self.pos_aim.x > self.pos.x :
             self.pos.x += 1
         if self.pos_aim.x < self.pos.x :
@@ -21,3 +22,24 @@ class Camera():
             self.pos.y += 1
         if self.pos_aim.y < self.pos.y :
             self.pos.y -= 1
+        """
+
+        print(self.pos_aim.x-self.pos.x) # -WIDTH/2 à WIDTH/2
+        dif_x = self.pos_aim.x - self.pos.x
+
+        offset = self.calculOffset(dif_x)
+
+        self.pos.x += offset
+
+
+    def calculOffset(self,diff_x):
+
+        X = [-WIDTH/2,          -WIDTH/10,          0,          WIDTH/10,           WIDTH/2] # random x values
+        Y = [-100,              -1,                 0,          1,                  100] # random y values
+
+        # Finding the interpolation
+        y_interp = interp1d(X, Y)
+
+        rez = y_interp(diff_x)
+
+        return math.floor(rez)
