06b1aeab0909e2c88cdce8b09189a8f9056c2aae
JSB27lp <1conscienceofficiel@gmail.com>
2025-10-23 12:35:52 +0200

    vc

--- Camera.py
+++ Camera.py
@@ -3,21 +3,21 @@ from head import *
 class Camera():
     def __init__(self):
         self.pos = vec((-WIDTH/2, -HEIGHT/2))
+        self.pos_aim = vec((-WIDTH/2, -HEIGHT/2))
 
     def update(self,player):
-        self.updateCameraCenterSmooth(player)
+        self.pos_aim = vec(player.pos.x - WIDTH/2,player.pos.y - HEIGHT/2)
+        self.updateCameraCenterSmooth()
 
-    def updateCameraCenterSmooth(self,player):
-            camera_aim = vec(player.pos.x - WIDTH/2,player.pos.y - HEIGHT/2)
+    def updateCameraCenterSmooth(self):
+        self.pos_aim.x = round(self.pos_aim.x)
+        self.pos_aim.y = round(self.pos_aim.y)
 
-            camera_aim.x = round(camera_aim.x)
-            camera_aim.y = round(camera_aim.y)
-
-            if camera_aim.x > self.pos.x :
-                self.pos.x += 1
-            if camera_aim.x < self.pos.x :
-                self.pos.x -= 1
-            if camera_aim.y > self.pos.y :
-                self.pos.y += 1
-            if camera_aim.y < self.pos.y :
-                self.pos.y -= 1
+        if self.pos_aim.x > self.pos.x :
+            self.pos.x += 1
+        if self.pos_aim.x < self.pos.x :
+            self.pos.x -= 1
+        if self.pos_aim.y > self.pos.y :
+            self.pos.y += 1
+        if self.pos_aim.y < self.pos.y :
+            self.pos.y -= 1
